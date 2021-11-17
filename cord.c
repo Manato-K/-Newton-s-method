b05b1422ccf1f58a4c5eeaef8ad39826627206b8
chrom-P <kkllmm104@gmail.com>
2021-11-17 10:45:39 +0900

    update

--- cord.c
+++ cord.c
@@ -3,23 +3,23 @@
 
 double f(double x)
 {
-    return(x*x-2.0);
+    return(x*x*x*x-6.0);
 }
 
 double f1(double x)
 {
-    return(2.0*x);
+    return(4*x*x*x);
 }
 int main(void)
 {
     int n=0;
     double x,x1,er=1.0e-9;
     
-    x1=2.0;
+    x1=1.5;
     do{
         x=x1;
         if(fabs(f1(x))<er) break;
         n++;
-        printf("%2d : %.10lf(%.10lf)\n", n,x1,f(x1));
+        printf("%2d : %.6lf(%.6lf)\n", n,x1,f(x1));
     } while(fabs(x1-x)>er);
 }
