7aeeed370e261c9b7cf889a27e6fe5a0d19c4d17
chrom-P <kkllmm104@gmail.com>
2021-12-22 09:54:28 +0900

    result

--- cord.c
+++ cord.c
@@ -1,30 +1,5 @@
 #include <stdio.h>
 #include <math.h>
-
-double f(double x)
-{
-    return(x*x*x*x-6.0);
-}
-
-double f1(double x)
-{
-    return(4.0*x*x*x);
-}
-int main(void)
-{
-    int n=0;
-    double x,x1,er=1.0e-9;
-    
-    x1=1.5;
-    do{
-        x=x1;
-        if(fabs(f1(x))<er) break;
-        n++;
-        printf("%2d : %.6lf(%.6lf)\n", n,x1,f(x1));
-    } while(fabs(x1-x)>er);
-}
-#include <stdio.h>
-#include <math.h>
 double f(double x)
 {   
     return(x*x*x*x-6.0);
